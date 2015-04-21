30fcbf12ecafef249910cd7428a2855bbe0ce1d1
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-21 15:01:20 +0100

    dish done

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,6 +1,11 @@
-require 'Dish'
+require 'dish'
 
 describe Dish do
-  xit '' do
+  dish = Dish.new('pizza', 5)
+  it 'has a name' do
+    expect(dish.name).to eq 'pizza'
+  end
+  it 'has a price' do
+    expect(dish.price).to eq 5
   end
 end
